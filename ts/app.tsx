f1463abb2ab681cd8167967c02871dcd2af7967d
Edward Glendinning <edward82009@hotmail.com>
2022-01-26 21:23:04 +0000

    Basic Styling

--- ts/app.tsx
+++ ts/app.tsx
@@ -1,6 +1,29 @@
 import * as React from 'react';
 import DataDisplay from './components/data-display';
+import styled from 'styled-components';
 import type { Stats } from './utils/types';
+
+const ContainerDiv = styled.div`
+  display: grid;
+  width: 100%;
+  height: 100vh;
+  grid-template-columns: 5% auto 5%;
+  font-family: 'Titillium Web', sans-serif;
+  font-weight: 400;
+
+  h1 {
+    font-family: 'Titillium Web', sans-serif;
+    font-weight: 600;
+  }
+`;
+
+const HeaderRow = styled.div`
+  display: flex;
+  text-align: center;
+  grid-row: 1;
+  grid-column: 2;
+  flex-direction: column;
+`;
 
 const App = (): JSX.Element => {
   const [ stats, setStats ] = React.useState<Stats>({});
@@ -24,14 +47,15 @@ const App = (): JSX.Element => {
   }, []);
 
   return (
-    <>
-      <h1>{'Welcome to React Stats!'}</h1>
-      <h2>{'System Info:'}</h2>
+    <ContainerDiv>
+      <HeaderRow>
+        <h1>{'Welcome to React Stats!'}</h1>
+      </HeaderRow>
       {
         Object.keys(stats).length ?
           <DataDisplay data={stats} /> : null
       }
-    </>
+    </ContainerDiv>
   );
 };
 
--- ts/components/data-display.tsx
+++ ts/components/data-display.tsx
@@ -1,31 +1,46 @@
 import * as React from 'react';
 import ReactSpeedometer from 'react-d3-speedometer';
+import styled from 'styled-components';
 import type { Stats } from '../utils/types';
 
 interface Props {
   data: Stats;
 }
 
+const DataContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  grid-column: 2;
+  grid-row: 2;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
 const DataDisplay = ({ data }: Props): JSX.Element => {
   return (
-    <>
+    <DataContainer>
+      <Row>
+        <ReactSpeedometer
+          minValue={0}
+          maxValue={100}
+          value={data.usage}
+          currentValueText={`CPU Usage: ${data.usage}%`}
+        />
+        <ReactSpeedometer
+          minValue={0}
+          maxValue={data.freemem?.totalMemMb}
+          value={data.freemem?.freeMemMb}
+          currentValueText={`RAM Usage: ${data.freemem?.freeMemMb}.`}
+        />
+      </Row>
       <p>{`CPU Model: ${data.cpu_platform}.`}</p>
       <p>{`CPU core count: ${data.core_count}.`}</p>
-      <ReactSpeedometer
-        minValue={0}
-        maxValue={100}
-        value={data.usage}
-        currentValueText={`CPU Usage: ${data.usage}%`}
-      />
-      <p>{`RAM: ${data.freemem?.freeMemMb}/${data.freemem?.totalMemMb}.`}</p>
-      <ReactSpeedometer
-        minValue={0}
-        maxValue={data.freemem?.totalMemMb}
-        value={data.freemem?.freeMemMb}
-        currentValueText={`RAM Usage: ${data.freemem?.freeMemMb}.`}
-      />
       <p>{`Server uptime: ${data.uptime}.`}</p>
-    </>
+    </DataContainer>
   );
 }
 
