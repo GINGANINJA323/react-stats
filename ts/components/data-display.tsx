eecebe380046946cd3d2716c8afee4ebf10faccc
Edward Glendinning <edward82009@hotmail.com>
2022-02-12 15:14:22 +0000

    Stricter tsconfig, added JSON module resolution

--- ts/components/data-display.tsx
+++ ts/components/data-display.tsx
@@ -23,19 +23,6 @@ const Row = styled.div`
 const DataDisplay = ({ data }: Props): JSX.Element => {
   const memoryInUse = Math.round(data.freemem.totalMemMb) - Math.round(data.freemem.freeMemMb);
 
-  const getEvenMemorySegments = (totalMemory: number): Array<number> => {
-    const segmentNumbers = [0];
-
-    for (let i = 0; i < 5; i++) {
-      const segmentNum = Math.round(totalMemory) / 5 * i;
-      segmentNumbers.push(Math.round(segmentNum));
-    }
-
-    segmentNumbers.push(Math.round(totalMemory));
-
-    return segmentNumbers;
-  }
-
   const formatDuration = (time: number): string => {
     // from https://www.codegrepper.com/code-examples/javascript/convert+seconds+to+hours+minutes+seconds+javascript
     const hours   = Math.floor(time / 3600); // get hours
